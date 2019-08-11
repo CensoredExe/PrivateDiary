a0f394dfa9b50319833ed32093602697f5f05764
Oliver Crowe <olivercampbellcrowe@outlook.com>
2019-08-11 18:22:46 +0100

    Initial files, begin landing page frontend

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+    session_start();
+    
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Meta tags -->
+        <title>Private Diary - Keep a personal, completely private diary for yourself.</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <header>
+            <div class="text-box">
+                <h1>Keep a personal, private diary - online.</h1>
+            </div>
+        </header>
+        <br><br>
+    </body>
+</html>
